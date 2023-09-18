4eaae4a251bb14add7e1cda8d1b32d820fe81df4
Bartosz Geodecki <1p22geodecki@gmail.com>
2023-09-18 08:00:41 +0200

    chore(samochodzik): Moved function definitions away from class declaration.
    Preparing to move class declaration into header file.

--- main.cpp
+++ main.cpp
@@ -4,40 +4,61 @@ using namespace std;
 
 class Samochodzik;
 
-class Samochodzik{
-    public:
-        string name;
-        string color;
-        void load(float weight){
-            if(weight>0){
-                this->loadWeight += weight;
-            }
-            else{
-                throw "Negative weight";
-            }
-        }
-        void unload(float weight){
-            if((weight>0)&&(this->loadWeight>=weight)){
-                this->loadWeight-=weight;
-            }
-            else{
-                throw "Negative weight";
-            }
-        }
-        float getWeight(){
-            return this->weight+this->loadWeight;
-        }
-        string getNumber(){
-            return this->regNumber;
-        }
-    private:
-        float loadWeight = 0;
-        float weight = 1000; // domyślna waga to 1t
-    protected:
-        string regNumber = "XXXX";
-        //dostępne w klasach pochodych
+class Samochodzik
+{
+public:
+    string name;
+    string color;
+
+    void load(float weight);
+    void unload(float weight);
+
+    float getWeight();
+
+    string getNumber();
+
+private:
+    float loadWeight = 0;
+    float weight = 1000; // domyślna waga to 1t
+protected:
+    string regNumber = "XXXX";
+    // dostępne w klasach pochodych
 };
 
+
+void Samochodzik::load(float weight)
+{
+    if (weight > 0)
+    {
+        this->loadWeight += weight;
+    }
+    else
+    {
+        throw "Negative weight";
+    }
+}
+
+void Samochodzik::unload(float weight)
+{
+    if ((weight > 0) && (this->loadWeight >= weight))
+    {
+        this->loadWeight -= weight;
+    }
+    else
+    {
+        throw "Negative weight";
+    }
+}
+
+float Samochodzik::getWeight()
+{
+    return this->weight + this->loadWeight;
+}
+
+string Samochodzik::getNumber()
+{
+    return this->regNumber;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -45,10 +66,10 @@ int main(int argc, char const *argv[])
     Samochodzik multipla;
     multipla.name = "VW Multipla";
     multipla.color = "czerwony";
-    multipla.load(12); //ładujemy 12 kg
-    multipla.unload(8); //wyciągamy 8 kg
+    multipla.load(12);  // ładujemy 12 kg
+    multipla.unload(8); // wyciągamy 8 kg
     multipla.load(24);
-    cout <<"Multipla wazy "<< multipla.getWeight()<<"kg"<<endl; // polskie znaki nie wchodzą;
-    cout<<"Numer Multipli to: "<<multipla.getNumber()<<endl;
+    cout << "Multipla wazy " << multipla.getWeight() << "kg" << endl; // polskie znaki nie wchodzą;
+    cout << "Numer Multipli to: " << multipla.getNumber() << endl;
     return 0;
 }
