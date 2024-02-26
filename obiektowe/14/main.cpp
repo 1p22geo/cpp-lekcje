7f810486e5426b7a4cefc3810aad9831433430ce
Bartosz Geodecki <1p22geodecki@gmail.com>
2024-02-26 08:15:31 +0100

    feat: error handling with error codes

--- obiektowe/14/main.cpp
+++ obiektowe/14/main.cpp
@@ -5,8 +5,12 @@ int main(int argc, char const *argv[]){
     std::cout<<"Podaj boki trojkota jako liczby calkowite"<<std::endl;
     std::cin>>a>>b>>c;
     
+    if( (a+b<=c)||(a+c<=b)||(c+b<=a) ){
+        std::cout<<"e byku ale to nie ma trojkot"<<std::endl;
+        return 1;
+    }
+
     float p = (a+b+c)/2.0;
-    std::cout<<p;
     float S = sqrt(p * (p-a) * (p-b) * (p-c));
 
     std::cout<<std::endl<<"Pole trojkota to "<<S<<std::endl;
