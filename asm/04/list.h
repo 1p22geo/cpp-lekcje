bb855b4e94a4a9926aa7cbc09be1048996c1dfaa
1p22geo <1p22geodecki@gmail.com>
2024-04-05 12:19:53 +0200

    feat: C socket example

--- asm/04/list.h
+++ asm/04/list.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <stdint.h>
 
-
 struct Node {
   uint8_t value;
   struct Node *next;
@@ -12,9 +11,8 @@ struct List {
   uint32_t len;
 };
 
-
-struct Node * create_node(uint8_t value);
-struct List * create_list();
-void list_push(struct List * list, uint8_t value);
+struct Node *create_node(uint8_t value);
+struct List *create_list();
+void list_push(struct List *list, uint8_t value);
 uint8_t list_pop(struct List *list);
-uint8_t list_get(struct List * list, uint32_t index);
+uint8_t list_get(struct List *list, uint32_t index);
---
+++ asm/05/server.h
@@ -0,0 +1,10 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
